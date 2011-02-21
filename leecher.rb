072ee6b1b0d33be3351b3407d4c13734e2663fa7
Belén Albeza <belen.albeza@gmail.com>
2011-02-21 23:21:18 +0000

    first version of the script finished :)

--- leecher.rb
+++ leecher.rb
@@ -1,7 +1,11 @@
+# encoding: UTF-8
+
 require 'net/http'
 require 'erb'
 require 'iconv'
-require 'hpricot'
+require 'open-uri'
+require 'date'
+require 'nokogiri'
 
 class Hash
   def to_binding
@@ -11,91 +15,111 @@ class Hash
   end
 end
 
-class String
-  def to_utf8
-    ::Iconv.conv('UTF-8//IGNORE', 'UTF-8', self + ' ')[0..-2]
-  end
-end
-
 class Leecher
-  FicSourceURL = "www.fanfiction.net"
+  FicSourceURL = "http://www.fanfiction.net" 
   ChapterTemplate = 'chapter.html.erb'
   OpfTemplate = 'book.opf.erb'
+  TocTemplate = 'toc.html.erb'
   
   # constructor
-  def initialize(fic_id, chapter_range)
+  def initialize(fic_id, chapter_range, legal_mode=false)
     @fic_id = fic_id
     @chapter_range = chapter_range
     @chapters = []
     @opf = nil
+    @legal_mode = legal_mode
   end
   
   def run
-    puts $KCODE
+    # get story info
+    puts "Fetching http://www.fanfiction.net/s/#{@fic_id}"
+    story_meta = leech_story_info
+    
+    # generate chapters
+    @titles = []
     for i in @chapter_range do
-      raw = leech_chapter(i)
-      raw[:index] = 1
+      # a pause is needed to comply with FF.net TOS
+      # plese read README file to more information about the TOS
+      if @legal_mode
+        puts "Making a 30 seconds pause to comply with fanfiction.net TOS..."
+        puts "Read README file for more information about this issue"
+        sleep 30
+      end
       
-      file = Tempfile.new("chapter#{i}.html", ".")
-      file << render_chapter(raw)
-      @chapters.push(file.path)
+      raw = leech_chapter(i)
+      raw[:index] = i
+      
+      puts "Parsing chapter #{i}"
+      create_html_file("chapter#{i}.html", ChapterTemplate, raw)
+              
+      @chapters.push("chapter#{i}.html")
+      @titles.push(raw[:title])    
     end
     
-    @opf = render_opf({
+    # generate TOC
+    create_html_file("toc.html", TocTemplate, {
       :chapters => @chapters,
-      :title => "This is the fic title",
-      :lang => "en",
-      :author => "Anonymous",
-      :publisher => "",
-      :pub_date => Date.today.to_s,
-      :description => ""
+      :titles => @titles,
+      :story_title => story_meta[:title],
+      :story_author => story_meta[:author]
     })
     
-    
-    opf_file = File.new("book.opf", "w+")
-    opf_file << @opf
-    opf_file.close
-    
-    puts File.exist?(@chapters.first)
-    
-    
+    # generate OPF file
+    create_html_file("book.opf", OpfTemplate, {
+      :chapters => @chapters,
+      :title => story_meta[:title],
+      :lang => "en",
+      :author => story_meta[:author],
+      :toc => "toc.html"
+    })
+        
+    puts "Generating ebook..."
     puts `kindlegen book.opf -unicode`
+    
+    cleanup
   end
   
   protected
   
-  # downloads & process a single chapter
-  def leech_chapter(index)
-    puts "Leeching chapter \##{index}..."
-    Net::HTTP.start(FicSourceURL) do |http|
-      # get page and convert it to a Hpricot doc
-      page = http.get("/s/#{@fic_id}/#{index}").body
-      doc = Hpricot(page)
-      
-      # get chapter content inside #storytext
-      content = doc.search('//div#storytext').first.html
-      # get chapter title from a dropdown menu
-      # chapter titles in FF.net are written with this format: "28. This is a chapter"
-      title = doc.search("//option[@value='#{index}']").first.inner_text.gsub(/^\d+\. /, "")
-      
-      {:content => content.to_utf8, :title => title.to_utf8}
+  # creates a HTML file using the template and data provided
+  def create_html_file(filename, template, data)
+    File.open(filename, "w+:utf-8") do |file|
+      file << ERB.new(File.read(template)).result(data.to_binding)
     end
   end
   
-  # renders a chapter using a ERB template
-  def render_chapter(data)
-    template = ERB.new(File.read(ChapterTemplate))
-    template.result(data.to_binding).to_utf8
+  # gets the book info from the front chapter in the story
+  def leech_story_info
+    doc = Nokogiri::HTML(open("#{FicSourceURL}/s/#{@fic_id}"), "UTF-8")    
+    # get author name
+    author = doc.xpath("//body/div[13]/table[1]//a").first.inner_text
+    # get story title
+    title = doc.xpath("//body/div[13]/div[1]//b").first.inner_text
+    
+    {:title => title, :author => author}
   end
   
-  # renders the OPF
-  def render_opf(data)
-    template = ERB.new(File.read(OpfTemplate))
-    template.result(data.to_binding).to_utf8
+  # downloads & process a single chapter
+  def leech_chapter(index)
+    # puts "Leeching chapter \##{index}..."
+    doc = Nokogiri::HTML(open("#{FicSourceURL}/s/#{@fic_id}/#{index}"), "UTF-8")
+    
+    # get chapter content inside #storytext
+    content = doc.xpath('//div[@id="storytext"]').first.inner_html
+    # get chapter title from a dropdown menu
+    # chapter titles in FF.net are written with this format: "28. This is a chapter"
+    title = doc.xpath("//option[@value='#{index}']").first.inner_text.gsub(/^\d+\. /, "")
+    
+    {:content => content, :title => title}
   end
   
-  def generate_ebook
-    # dump the OPF template to a temp file
-    opf_file = Tempfile.new
+  # cleans up temporary files
+  def cleanup
+    filenames = ["book.opf", "toc.html"]
+    filenames += @chapters
+    
+    filenames.each do |filename|
+      File.delete(filename)
+    end
   end
 end
